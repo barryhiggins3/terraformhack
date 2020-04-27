4300759e7354b0e5b8b9220ad741a62ff56c7ee1
Matt McEvoy <Matt.McEvoy@modalitysystems.com>
2020-04-27 10:07:14 +0100

    commit to master

--- modules/securitycentre/main.tf
+++ modules/securitycentre/main.tf
@@ -17,3 +17,14 @@ resource "azurerm_security_center_workspace" "security_center_workspace" {
 resource "azurerm_security_center_subscription_pricing" "security_center_subscription_pricing" {
   tier = "Standard"
 }
+resource "azurerm_log_analytics_solution" "solution-sentinel" {
+  solution_name         = "SecurityInsights"
+  location              = var.location
+  resource_group_name   = azurerm_resource_group.resource_group.name
+  workspace_resource_id = azurerm_log_analytics_workspace.log_analytics_workspace.id
+  workspace_name        = azurerm_log_analytics_workspace.log_analytics_workspace.name
+  plan {
+    publisher = "Microsoft"
+    product   = "OMSGallery/SecurityInsights"
+  }
+}
